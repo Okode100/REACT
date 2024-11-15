f14c114eee8b59648d963c28bb39076b308c6409
Moses Okode <okodekings76@gmail.com>
2024-11-15 21:55:20 +0300

    added react components

---
+++ Header.js
@@ -0,0 +1,13 @@
+ export default function Header(){
+    return (
+        <header> 
+        <nav className = "nav">
+            <img src="./react logo.jpeg" className="nav-logo" alt="logo"/>
+            <ul className="nav-items" >
+                <li>Home</li>
+                <li>Services</li>
+                <li>About</li>
+                    <li>Contact</li>
+                    </ul>
+                </nav>
+    </header>
--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-ReactDOM.render(
-    <h1>Hello, React! I'm going to learn you</h1>,
-    document.getElementById("root")
-);
+function Header(){
+    return (
+        <header> 
+        <nav className = "nav">
+            <img src="./react logo.jpeg" className="nav-logo" alt="logo"/>
+            <ul className="nav-items" >
+                <li>Home</li>
+                <li>Services</li>
+                <li>About</li>
+                    <li>Contact</li>
+                    </ul>
+                </nav>
+    </header>
+
+    );
+}
+function Page()  {
+    return (        
+    <div> 
+       
+        <h1>These are the think i like</h1>
+        <ul> 
+            <li>Coding in the morning </li>
+            <li>Coffe at night before i go to code more</li>
+            <li>I loike cooked cabbage when am coding through my projects</li>
+        </ul>
+        <p>This are the thing that i love doing</p>
+        <footer> <small>@ 20xx Okode development. All the rights reserved</small></footer>
+    </div>
+
+    );
+};
+ReactDOM.render(<Page />, document.getElementById("root"));
