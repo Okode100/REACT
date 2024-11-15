680f58ac7d40aa9d229e0d3dc6770e3b9c9f4b80
Moses Okode <okodekings76@gmail.com>
2024-11-15 22:00:24 +0300

    changed the component to footer

---
+++ Footer.js
@@ -0,0 +1,11 @@
+export default function footer() {
+    return(
+        <footer>
+             <small>@ 20xx Okode development. All the rights reserved</small>
+             
+             </footer>
+        
+    
+
+    );
+}
--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-function Header(){
-    return (
-        <header> 
-        <nav className = "nav">
-            <img src="./react logo.jpeg" className="nav-logo" alt="logo"/>
-            <ul className="nav-items" >
-                <li>Home</li>
-                <li>Services</li>
-                <li>About</li>
-                    <li>Contact</li>
-                    </ul>
-                </nav>
-    </header>
+import Header from "./Header";
+import Footer from "./Footer";
 
-    );
-}
+
 function Page()  {
     return (        
     <div> 
@@ -25,9 +13,8 @@ function Page()  {
             <li>I loike cooked cabbage when am coding through my projects</li>
         </ul>
         <p>This are the thing that i love doing</p>
-        <footer> <small>@ 20xx Okode development. All the rights reserved</small></footer>
-    </div>
-
+       
     );
+    </div>
 };
 ReactDOM.render(<Page />, document.getElementById("root"));
